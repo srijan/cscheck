198fc0458653bcba528fad82b3e7e7acdbc20e9e
Srijan Choudhary <srijan4@gmail.com>
2012-02-08 16:35:57 +0530

    Now list is printed to a file, and script runs every 1 min.

--- cs.py
+++ cs.py
@@ -2,24 +2,23 @@
 import Queue
 import socket
 import threading
+import time
 from netaddr import IPNetwork
 
 socket.setdefaulttimeout(0.05)
 
-def parsing(txt):
+def parsing(txt,ip):
     txt=txt.replace('\377', '')
     if txt.find('m') == 0:
         serv_name=txt.split('\0') [1]
         serv_map=txt.split('\0') [2]
         serv_engine=txt.split('\0') [3]
         serv_game=txt.split('\0') [4]
-    print '\t\tServer name:', serv_name
-    print '\t\tGame:', serv_game, '('+serv_engine+')'
-    print '\t\tMap:', serv_map
+    global serverList
+    serverList.append(ip+"\t:\t"+serv_name+" -- "+serv_map)
 
 class ClientThread (threading.Thread):
     def run (self):
-        excp = False
         ip = None
         while True:
             if ipPool.qsize() > 0:
@@ -27,31 +26,25 @@ class ClientThread (threading.Thread):
             else:
                 break
             if ip != None:
-                excp = False
                 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 try:
                     sock.connect((ip, 27015))
                 except Exception,e:
                     sock.close()
-                    print ip, e
+                    print e
                     break
                 sock.send('\377\377\377\377TSource Engine Query\0')
                 while 1:
                     try:
                         text=sock.recv(1024)
                     except Exception,e:
-                        #print ip + " : " , e
-                        excp = True
                         break
-                    print ip+" : "
-                    text=parsing(text)
+                    text=parsing(text,ip)
                     if not text:
                         break
                     print '[GET]', text
                 sock.shutdown(socket.SHUT_RDWR)
                 sock.close()
-                if not excp:
-                    print ' '
 
 subnetList = [
                 "172.16.1.0/24",
@@ -78,21 +71,29 @@ subnetList = [
                 "172.17.2.0/24"
              ]
 ipPool = Queue.Queue(0)
-
+global serverList
+serverList = []
+fileName = "/srv/http/cs.txt"
 
 def checkIPs():
+    global serverList
+    serverList = ["","CS Servers List: (Auto updated every minute)",""]
     for subnet in subnetList:
         for ip in IPNetwork(subnet).iter_hosts():
             ipPool.put('%s' % ip)
-
-    for x in xrange(100):
+    for x in xrange(50):
         ClientThread().start()
+    while threading.activeCount() > 1:
+        time.sleep(1)
 
-checkIPs()
 while True:
-    if ipPool.empty():
-        print '--End--'
-        # write file
-        # wait 1 min
-        # redo..
-        break
+    checkIPs()
+    serverList.append("")
+    serverList.append("Last updated at: "+time.strftime('%I:%M %p, %b %d, %Y'))
+    print time.strftime('%I:%M %p, %b %d, %Y'), '-- MARK --'
+    f = open(fileName, "w")
+    for s in serverList:
+        f.write(s)
+        f.write('\n')
+    f.close()
+    time.sleep(60)
